1c5095b9a9768554fbac42b2f411f348840e9237
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-04-20 16:24:12 +0300

    Updates

---
+++ precache-manifest.c44cd29809dc89a5c6686c2250b8de46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45627be436e994b1d4740716f676d19e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1f28112e3719ac107a95",
+    "url": "/goit-react-hw-01-components/static/css/main.cdcb20ef.chunk.css"
+  },
+  {
+    "revision": "0c4274e835fc97939a1d",
+    "url": "/goit-react-hw-01-components/static/js/2.43e647a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.43e647a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f28112e3719ac107a95",
+    "url": "/goit-react-hw-01-components/static/js/main.1693f5d9.chunk.js"
+  },
+  {
+    "revision": "3ae19d7715e4f233136b",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7fcbb96b.js"
+  }
+]);
